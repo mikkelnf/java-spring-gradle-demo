26523661658e0325eec1825feb2b98ded7360c70
MikkelNf <mikkel.nf@gmail.com>
2023-02-06 15:21:54 +0700

    first init

---
+++ src/main/java/com/example/demo/exception/BookIdMismatchException.java
@@ -0,0 +1,13 @@
+package com.example.demo.exception;
+
+public class BookIdMismatchException extends RuntimeException {
+
+    public BookIdMismatchException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    // ...
+
+
+    public BookIdMismatchException() {
+    }
+}
